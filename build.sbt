0cad64fba54d810fbb30537bdb86e5709b8195ef
Artur <fowlart1988@gmail.com>
2022-01-19 13:55:15 +0200

    added maters to the Aggregations

--- build.sbt
+++ build.sbt
@@ -7,13 +7,13 @@ scalaVersion := "2.12.10"
 val sparkVersion = "3.0.2"
 val vegasVersion = "0.3.11"
 val postgresVersion = "42.2.2"
+val hadoopVersion = "3.3.1"
 
 resolvers ++= Seq(
   "bintray-spark-packages" at "https://dl.bintray.com/spark-packages/maven",
   "Typesafe Simple Repository" at "https://repo.typesafe.com/typesafe/simple/maven-releases",
   "MavenRepository" at "https://mvnrepository.com"
 )
-
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
@@ -22,5 +22,6 @@ libraryDependencies ++= Seq(
   "org.apache.logging.log4j" % "log4j-api" % "2.4.1",
   "org.apache.logging.log4j" % "log4j-core" % "2.4.1",
   // postgres for DB connectivity
-  "org.postgresql" % "postgresql" % postgresVersion
+  "org.postgresql" % "postgresql" % postgresVersion,
+  "com.fasterxml.jackson.core" % "jackson-databind" % "2.10.0"
 )
--- src/main/scala/part2_dataframes/Aggregations.scala
+++ src/main/scala/part2_dataframes/Aggregations.scala
@@ -32,7 +32,8 @@ object Aggregations extends App {
   // quick count
   moviesDF.select(approx_count_distinct("Major_Genre"))
 
-  // min/max/sum/avg/mean/stddev
+  /** Here are some of the built-in functions available for aggregation, without groupBy call.
+   * 'import org.apache.spark.sql.functions._' required. */
   moviesDF.select(
     avg(col("US_Gross")),
     min(col("Worldwide_Gross")),
@@ -40,17 +41,33 @@ object Aggregations extends App {
     sum(col("US_Gross")),
     mean(col("Worldwide_Gross")),
     stddev(col("Rotten_Tomatoes_Rating")),
-    countDistinct("Distributor")
+    countDistinct("Distributor"),
+    corr("Worldwide_Gross", "US_Gross"),
+    collect_set("Distributor")
   )
 
   moviesDF.selectExpr("avg(US_Gross)")
 
-  // grouping
-  // only one aggregation ?
-  val usGrossForEachGenre = moviesDF.groupBy(col("Major_Genre")).sum("US_Gross")
-  val groupedByRating = moviesDF.groupBy(col("MPAA_Rating").as("Distinct rating groups")).count()
-  groupedByRating.show()
+  /** Aggregate built-in functions. GroupBy returns RelationalGroupedDataset. */
+  val sumUsGrossForEachGenre = moviesDF.groupBy(col("Major_Genre")).sum("US_Gross")
+  val maxGrossForEachGenre = moviesDF.groupBy(col("Major_Genre")).max("US_Gross")
+  val avgGrossForEachGenre = moviesDF.groupBy(col("Major_Genre")).avg("US_Gross")
+  // ???
+  val x = moviesDF.groupBy(col("Major_Genre")).pivot("US_Gross")
 
+  // Will show a sum by each Major_Genre group(and 'Major_Genre' values as well)
+  // sumUsGrossForEachGenre.show()
 
+  // Grouping by multiply columns:
+  moviesDF
+    .groupBy(col("Major_Genre"), col("Distributor"))
+    .avg("US_Gross")
+    .orderBy(col("Major_Genre").desc)
+    .show()
 
+  // Applying function above groupBy to specify aggregation
+  // Apply multiple aggregate functions on grouped data
+  moviesDF
+    .groupBy(col("Major_Genre"), col("Distributor"))
+    .agg(sum("US_Gross").as("sum of US_Gross"), avg("US_Gross").as("avg of US_Gross"))
 }
